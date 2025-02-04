4f538baf4e5ac45eef6a6b59085e5f6bb3d05d5f
Samyukt Sriram <sriramsamyukt@gmail.com>
2025-02-04 15:13:07 +0100

    basic helper scripts

---
+++ preprocessing_scripts/encoding_ylabels.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+# Define the encodings dictionary
+encodings = {
+    'GOOD': 0,
+    'Boucle plate': 1,
+    'Lift-off blanc': 2,
+    'Lift-off noir': 3,
+    'Missing': 4,
+    'Short circuit MOS': 5
+    # Add more label encodings as needed
+}
+
+# Path to the CSV file
+csv_file_path = '/Users/sam/Desktop/X/ens100/data_large_files/Y_train.csv'
+
+# Load the CSV file into a DataFrame
+df = pd.read_csv(csv_file_path)
+
+# Specify the column to encode
+column_to_encode = 'Label'
+
+# Encode the labels using the encodings dictionary
+df[column_to_encode] = df[column_to_encode].map(encodings)
+
+# Save the encoded DataFrame back to a CSV file
+df.to_csv('/Users/sam/Desktop/X/ens100/data_large_files/Y_train.csv', index=False)
